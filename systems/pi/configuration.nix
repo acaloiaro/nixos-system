7bd7354db7d66158406b0ee62a258a90df0b12e7
Adriano Caloiaro <code@adriano.fyi>
2023-10-14 19:19:05 -0600

    pi: Enable tailscale

--- systems/pi/configuration.nix
+++ systems/pi/configuration.nix
@@ -27,6 +27,16 @@
     };
   };
 
+  # Configure secrets 
+  age = {
+    identityPaths = [ "/home/pi/.ssh/id_rsa" ];
+    secrets = {
+      tailscale_key = {
+        file = ./secrets/tailscale_key.age;
+      };
+    };
+  };
+
   sound.enable = true;
   hardware = {
     enableRedistributableFirmware = true;
@@ -90,12 +100,27 @@
   networking = {
     hostName = "roampi"; # Define your hostname.
     hostId = "0cb3361a";
-    firewall.allowedTCPPorts = [ 22 ];
+
+    firewall = {
+      # enable the firewall
+      enable = true;
+
+      # always allow traffic from your Tailscale network
+      trustedInterfaces = [ "tailscale0" ];
+
+      # allow the Tailscale UDP port through the firewall
+      allowedUDPPorts = [ config.services.tailscale.port ];
+
+      # allow you to SSH in over the public internet
+      allowedTCPPorts = [ 22 ];
+    };
   };
 
   environment.systemPackages = with pkgs; [
      git 
+     jq
      kitty
+     tailscale
      vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
      wget
      libcec
@@ -159,6 +184,38 @@
     };
   };
 
+  # Enable tailscaled 
+  services.tailscale.enable = true;
+
+  # create a oneshot job to authenticate to Tailscale
+  systemd.services.tailscale-autoconnect = {
+    description = "Automatic connection to Tailscale";
+    enable = true;
+
+    # make sure tailscale is running before trying to connect to tailscale
+    after = [ "network-pre.target" "tailscale.service" ];
+    wants = [ "network-pre.target" "tailscale.service" ];
+    wantedBy = [ "multi-user.target" ];
+
+    # set this service as a oneshot job
+    serviceConfig.Type = "oneshot";
+
+    # have the job run this shell script
+    script = with pkgs; ''
+      # wait for tailscaled to settle
+      sleep 2
+
+      # check if we are already authenticated to tailscale
+      status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
+      if [ $status = "Running" ]; then # if so, then do nothing
+        exit 0
+      fi
+
+      # otherwise authenticate with tailscale
+      ${tailscale}/bin/tailscale up -authkey $(cat ${config.age.secrets.tailscale_key.path}) --accept-routes --reset
+    '';
+  };
+ 
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
   # accidentally delete configuration.nix.
