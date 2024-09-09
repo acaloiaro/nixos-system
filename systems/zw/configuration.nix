799fb558478c5cda65e27c940949696bde4adca2
Adriano Caloiaro <code@adriano.fyi>
2024-09-09 09:10:35 -0700

    zw: use native tailscale auth file

--- systems/zw/configuration.nix
+++ systems/zw/configuration.nix
@@ -223,8 +223,10 @@
     services.pcscd.enable = true;
     services.dbus.enable = true;
     services.blueman.enable = true;
-
-    services.tailscale.enable = true;
+    services.tailscale = {
+      authKeyFile = config.age.secrets.tailscale_key.path;
+      enable = true;
+    };
     services.ivpn.enable = true;
     services.openntpd.enable = true;
     services.openssh = {
@@ -374,35 +376,6 @@
       debug = false;
       mode = "challenge-response";
       id = ["24654932"];
-    };
-
-    # create a oneshot job to authenticate to Tailscale
-    systemd.services.tailscale-autoconnect = {
-      description = "Automatic connection to Tailscale";
-      enable = true;
-
-      # make sure tailscale is running before trying to connect to tailscale
-      after = ["network-pre.target" "tailscale.service"];
-      wants = ["network-pre.target" "tailscale.service"];
-      wantedBy = ["multi-user.target"];
-
-      # set this service as a oneshot job
-      serviceConfig.Type = "oneshot";
-
-      # have the job run this shell script
-      script = with pkgs; ''
-        # wait for tailscaled to settle
-        sleep 2
-
-        # # check if we are already authenticated to tailscale
-        # status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
-        # if [ $status = "Running" ]; then # if so, then do nothing
-        #   exit 0
-        # fi
-
-        # otherwise authenticate with tailscale
-        ${tailscale}/bin/tailscale up -authkey $(cat ${config.age.secrets.tailscale_key.path}) --accept-routes --reset
-      '';
     };
 
     systemd.services.address-book-sync = {
@@ -569,11 +542,10 @@
           nix-index
           nodejs
           nodePackages.typescript-language-server
-          nomad_1_5
+          nomad_1_6
           nil # nix lsp
           opentofu
           playerctl
-          python310Packages.python-lsp-server
           python311Packages.sqlparse
           ripgrep
           rofi
