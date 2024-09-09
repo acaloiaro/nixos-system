a220e2dcd53d12e662c6a12dabd88e3f9ccdc243
Adriano Caloiaro <code@adriano.fyi>
2024-09-09 09:10:06 -0700

    hombee: use native tailscale auth file

--- systems/homebee/configuration.nix
+++ systems/homebee/configuration.nix
@@ -41,11 +41,6 @@
     };
   };
 
-  sound.enable = true;
-  hardware = {
-    pulseaudio.enable = true;
-  };
-
   nix = {
     package = pkgs.nixFlakes;
     extraOptions = ''
@@ -92,7 +87,11 @@
       };
     };
     adguardhome.enable = true;
-    tailscale.enable = true;
+    tailscale = {
+      enable = true;
+      authKeyFile = config.age.secrets.tailscale_key.path;
+      extraSetFlags = ["--advertise-exit-node"];
+    };
     jellyfin = {
       enable = true;
       openFirewall = true;
@@ -172,35 +171,6 @@
       packages = with pkgs; [
       ];
     };
-  };
-
-  # create a oneshot job to authenticate to Tailscale
-  systemd.services.tailscale-autoconnect = {
-    description = "Automatic connection to Tailscale";
-    enable = true;
-
-    # make sure tailscale is running before trying to connect to tailscale
-    after = ["network-pre.target" "tailscale.service"];
-    wants = ["network-pre.target" "tailscale.service"];
-    wantedBy = ["multi-user.target"];
-
-    # set this service as a oneshot job
-    serviceConfig.Type = "oneshot";
-
-    # have the job run this shell script
-    script = with pkgs; ''
-      # wait for tailscaled to settle
-      sleep 2
-
-      # check if we are already authenticated to tailscale
-      status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
-      if [ $status = "Running" ]; then # if so, then do nothing
-        exit 0
-      fi
-
-      # otherwise authenticate with tailscale
-      ${tailscale}/bin/tailscale up -authkey $(cat ${config.age.secrets.tailscale_key.path}) --accept-routes --advertise-routes=192.168.13.0/24 --reset
-    '';
   };
 
   # Copy the NixOS configuration file and link it from the resulting system
