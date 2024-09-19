f9ebd087340a38808fd897374e8b631cd347bc01
Adriano Caloiaro <code@adriano.fyi>
2024-09-19 08:12:23 -0700

    jellybee: configure tailscale the right way

--- systems/jellybee/configuration.nix
+++ systems/jellybee/configuration.nix
@@ -42,11 +42,6 @@
     };
   };
 
-  sound.enable = true;
-  hardware = {
-    pulseaudio.enable = true;
-  };
-
   nix = {
     package = pkgs.nixFlakes;
     extraOptions = ''
@@ -64,6 +59,11 @@
   time.timeZone = "America/Denver";
   i18n.defaultLocale = "en_US.UTF-8";
   services = {
+    adguardhome.enable = true;
+    tailscale = {
+      enable = true;
+      authKeyFile = config.age.secrets.tailscale_key.path;
+    };
     jellyfin = {
       enable = true;
       openFirewall = true;
@@ -133,38 +133,6 @@
       packages = with pkgs; [
       ];
     };
-  };
-
-  # Enable tailscaled
-  services.tailscale.enable = true;
-
-  # create a oneshot job to authenticate to Tailscale
-  systemd.services.tailscale-autoconnect = {
-    description = "Automatic connection to Tailscale";
-    enable = true;
-
-    # make sure tailscale is running before trying to connect to tailscale
-    after = ["network-pre.target" "tailscale.service"];
-    wants = ["network-pre.target" "tailscale.service"];
-    wantedBy = ["multi-user.target"];
-
-    # set this service as a oneshot job
-    serviceConfig.Type = "oneshot";
-
-    # have the job run this shell script
-    script = with pkgs; ''
-      # wait for tailscaled to settle
-      sleep 2
-
-      # check if we are already authenticated to tailscale
-      status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
-      if [ $status = "Running" ]; then # if so, then do nothing
-        exit 0
-      fi
-
-      # otherwise authenticate with tailscale
-      ${tailscale}/bin/tailscale up -authkey $(cat ${config.age.secrets.tailscale_key.path}) --accept-routes --advertise-routes=192.168.13.0/24 --reset
-    '';
   };
 
   services = {
