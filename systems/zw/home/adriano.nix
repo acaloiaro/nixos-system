dc7f16eb2ce4591325fb45c18a50433f70012204
Adriano Caloiaro <code@adriano.fyi>
2024-10-24 12:40:39 -0700

    weather: bishop

--- systems/zw/home/adriano.nix
+++ systems/zw/home/adriano.nix
@@ -192,7 +192,7 @@
         blocks = [
           {
             block = "custom";
-            command = "sed 's/  //' <(curl 'https://wttr.in/Santa%20Barbara,CA?format=4&u' -s)";
+            command = "sed 's/  //' <(curl 'https://wttr.in/Bishop,CA?format=4&u' -s)";
             interval = 1200;
           }
           {
