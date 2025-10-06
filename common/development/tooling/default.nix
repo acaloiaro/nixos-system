28d820a6e172e55c6821236e3123b016871594e4
Adriano Caloiaro <code@adriano.fyi>
2025-10-06 16:44:57 -0700

    darwin: tooling module refactor

--- common/development/tooling/default.nix
+++ common/development/tooling/default.nix
@@ -8,18 +8,24 @@ with lib;
 let
   cfg = config.tooling;
   stdenv = pkgs.stdenvNoCC;
+  brewBin = "/opt/homebrew/bin";
   brewFileName = ".Brewfile.greenhouse";
   brewFilePath = "$HOME/${brewFileName}";
+
+  # TODO Make configurable/option
   brewTaps = [
     "raggi/ale"
   ];
 
+  # TODO Make configurable/option
   brewBrews = [
-    "libpq@16"
     "raggi/ale/openssl-osx-ca"
+    "docker"
   ];
 
+  # TODO Make configurable/option
   brewCasks = [
+    "1password-cli"
   ];
 in
 {
@@ -56,6 +62,7 @@ in
       };
     };
   };
+  # TODO: Refactor the mkIf; this is goofy
   config = mkIf (isAttrs cfg.user) {
     home = {
       activation = {
@@ -67,51 +74,56 @@ in
           ${lib.getExe' pkgs.ruby "gem"} install --user-install --no-document tty-prompt 
         '';
 
-        configureRequiredSoftware = ''
-          export PATH="${
-            lib.makeBinPath (
-              with pkgs;
-              [
-                awscli2
-                cmake
-                curl
-                gh
-                imagemagick
-                mkcert
-                poppler
-                pkg-config
-                ruby
-                unrtf
-                wget
-              ]
-            )
-          }:$PATH:/usr/bin"
-        ''
-        + optionalString stdenv.isDarwin ''
-          # Mac only setup
-          #
-          # The following commands has a non-zero exit code, even if xcode is simply installed already.
-          # We're eating errors here, which is not ideal.
-          # TODO Find a more elegant solution
-          xcode-select --install &2>/dev/null
-        ''
-        + optionalString stdenv.isLinux ''
-          # Linux only setup
-        ''
-        + ''
-          # Make sure installed packages are in our PATH while this script runs
-          # Set up AWS
-          # ${lib.getExe' pkgs.curl "curl"} -o /tmp/aws-config-shim https://grnhse-vpc-assets.s3.amazonaws.com/installers/aws-config-shim
-          # ${lib.getExe' pkgs.ruby "ruby"} /tmp/aws-config-shim
-          # bundle config --global jobs $(${lib.getExe' pkgs.coreutils "nproc"} --all)
-          echo Please run "bundle config set --global https://rubygems.pkg.github.com/grnhse/ <GITHUB_USERNAME>:<TOKEN>"
-        '';
+        # onFilesChange is the homebrew activation that ensures homebrew packages are installed when Brewfile.greenhouse changes
+        configureRequiredSoftware = hm.dag.entryAfter [ "installSystemWideGems" "onFilesChange" ] (
+          ''
+            export PATH="${
+              lib.makeBinPath (
+                with pkgs;
+                [
+                  awscli2
+                  ruby
+                ]
+              )
+            }:$PATH:/usr/bin"
+          ''
+          + optionalString stdenv.isDarwin ''
+            # Mac only setup
+            #
+            # The following commands has a non-zero exit code, even if xcode is simply installed already.
+            # We're eating errors here, which is not ideal.
+            # TODO Find a more elegant solution
+            xcode-select --install &2>/dev/null
+          ''
+          + optionalString stdenv.isLinux ''
+            # Linux only setup
+          ''
+          + ''
+            # Make sure installed packages are in our PATH while this script runs
+            # Set up AWS
+            # ${lib.getExe' pkgs.curl "curl"} -o /tmp/aws-config-shim https://grnhse-vpc-assets.s3.amazonaws.com/installers/aws-config-shim
+            # ${lib.getExe' pkgs.ruby "ruby"} /tmp/aws-config-shim
+
+            # Enable 1password-cli (Enable integration in 1Password first, Settings -> Developer -> Integrate with 1Password CLI)
+            # TODO why isn't /opt/homebrew/bin in the PATH here?
+            eval $(/opt/homebrew/bin/op signin)
+
+            # Configure bundler to use Greenhouse's ruby gems repo.
+            # This requires you to have added a Github personal access token to 1password under your personal vault named "github-ruby-packages-pat", and the PAT must be in the 'password' field of the entry
+            # This also requires you to have logged in with the `gh` CLI: `gh auth login`
+            export PAT=$(/opt/homebrew/bin/op item get $(/opt/homebrew/bin/op item list --vault Employee | grep github-ruby-packages-pat | awk '{print $1}') --reveal --fields password)
+            export GH_USERNAME=$(${lib.getExe' pkgs.gh "gh"} api user --jq .login)
+            bundle config set --global https://rubygems.pkg.github.com/grnhse/ $GH_USERNAME:$PAT
+            bundle config --global jobs $(${lib.getExe' pkgs.coreutils "nproc"} --all)''
+        );
       };
 
       packages = with pkgs; [
+        asdf-vm
         awscli2
         cmake
         curl
+        duckdb
         gh
         imagemagick
         mkcert
@@ -123,14 +135,17 @@ in
         yarn
       ];
 
-      sessionPath = [ "/opt/homebrew/bin" ];
+      sessionPath = [
+        "/usr/bin"
+        "${brewBin}"
+        "$HOME/go/bin"
+      ];
       # These env vars are used by the AWS cli: https://greenhouseio.atlassian.net/wiki/spaces/SE/pages/710803589/2.+Secure+Credentials+Connections#AWS-CLI%2C-AWS-Shim
       sessionVariables = {
         AWS_DEFAULT_PROFILE = "dev.use1";
         AWS_PROFILE = "dev.use1";
         AWS_SDK_LOAD_CONFIG = "true";
         AWS_SESSION_TOKEN_TTL = "24h";
-        PATH = "$PATH:$home/.nix-profile/bin:$HOME/go/bin:/usr/bin";
       };
 
       file."${brewFileName}" = {
@@ -163,33 +178,47 @@ in
 
           ) brewCasks);
         onChange = ''
-          HOMEBREW_BUNDLE_FILE_GLOBAL=${brewFilePath} /opt/homebrew/bin/brew bundle install --cleanup --no-upgrade --force --global
+          HOMEBREW_BUNDLE_FILE_GLOBAL=${brewFilePath} ${brewBin}/brew bundle install --no-upgrade --force --global
         '';
       };
     };
 
-    programs.git = {
-      enable = true;
-      userName = cfg.user.name;
-      userEmail = cfg.user.email;
+    programs = {
+      git = {
+        enable = true;
+        userName = cfg.user.name;
+        userEmail = cfg.user.email;
 
-      signing = {
-        key = mkIf (cfg.user.gpg-key-id != "") cfg.user.gpg-key-id;
-        signByDefault = true;
+        signing = {
+          key = mkIf (cfg.user.gpg-key-id != "") cfg.user.gpg-key-id;
+          signByDefault = true;
+        };
+
+        extraConfig = {
+          push = {
+            autoSetupRemote = true;
+          };
+          init = {
+            defaultBranch = "main";
+          };
+          pull = {
+            rebase = true;
+          };
+          blame = {
+            ignoreRevsFile = ".git-blame-ignore-revs";
+          };
+        };
       };
 
-      extraConfig = {
-        push = {
-          autoSetupRemote = true;
+      gh = {
+        enable = true;
+
+        gitCredentialHelper = {
+          enable = true;
         };
-        init = {
-          defaultBranch = "main";
-        };
-        pull = {
-          rebase = true;
-        };
-        blame = {
-          ignoreRevsFile = ".git-blame-ignore-revs";
+
+        settings = {
+          git_protocol = "ssh";
         };
       };
     };
