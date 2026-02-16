86b648c204fbde2ac5ab592b4f3e600ae50972f9
Adriano Caloiaro <adriano.caloiaro@greenhouse.io>
2026-02-16 11:32:14 -0800

    chore: add devenv's cachix entry to accepted binary caches

--- common/binary-cache.nix
+++ common/binary-cache.nix
@@ -15,6 +15,7 @@ in {
       extra-substituters =
         [
           "https://colmena.cachix.org"
+          "https://devenv.cachix.org"
           "https://helix.cachix.org"
           "https://jj.cachix.org"
           "https://nixpkgs-ruby.cachix.org"
@@ -23,6 +24,7 @@ in {
       extra-trusted-public-keys =
         [
           "colmena.cachix.org-1:7BzpDnjjH8ki2CT3f6GdOk7QAzPOl+1t3LvTLXqYcSg="
+          "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
           "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
           "jj.cachix.org-1:rOXYMWIM8CAjR0M3kifWXVoDXVriTPz+gX5oSdJe2Is="
           "nixpkgs-ruby.cachix.org-1:vrcdi50fTolOxWCZZkw0jakOnUI1T19oYJ+PRYdK4SM="
